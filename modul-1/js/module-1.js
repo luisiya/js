29e7d8fc35cb394d2973b8c638d5d31f7e183c79
Kravchenko Luidmila <Luisiya@gmail.com>
2017-12-01 14:34:39 +0200

    modul-1

--- modul-1/js/module-1.js
+++ modul-1/js/module-1.js
@@ -5,24 +5,18 @@ const A  = FIRST.length;
 const B = SECOND.length;
 const C = THIRD.length;
 
-// console.log(A, B, C);
-
 var firstRow1 = FIRST.charAt(0);
 var firstRow2 = FIRST.charAt(A-1)
-// console.log(firstRow1, firstRow2);
 
 var secondRow1 = SECOND.charAt(0);
 var secondRow2 = SECOND.charAt(B-1)
-// console.log(secondRow1, secondRow2);
 
 var thirdRow1 = THIRD.charAt(0);
 var thirdRow2 = THIRD.charAt(C-1)
-// console.log(thirdRow1, thirdRow2);
 
 var row1 = FIRST.indexOf("b");
 var row2 = SECOND.indexOf("b");
 var row3 = THIRD.indexOf("b");
-// console.log(row1, row2, row3);
 
 
 console.log(`
@@ -40,6 +34,6 @@ console.log(`
 	- for third one are ${thirdRow1} and ${thirdRow2}.	
 
     Result	of finding symbol [b] is:
-    - [b] is located only in the third const and take index ${row3}.	`
+    - [b] is located only in the third const and takes index ${row3}.	`
 
 	);
