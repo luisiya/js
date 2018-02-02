16e4736b17a3480271150d77428225981105c4f8
Kravchenko Luidmila <Luisiya@gmail.com>
2018-02-02 15:06:32 +0200

    module-10_total

--- modul-10/js/module-10.js
+++ modul-10/js/module-10.js
@@ -2,7 +2,7 @@ const playSound = note => {
     const audio = document.querySelector(`audio[data-note=${note}]`);
     audio.currentTime = 0;
     audio.play();
-}
+};
 const note = ["do", "re", "mi", "fa", "sol", "la", "si"];
 const lang = {
     en: "qwertyuiop[]asdfghjkl;'zxcvbnm,./"
@@ -56,85 +56,127 @@ newObject.createLayout = function () {
 };
 newObject.createLayout();
 
-const buttons = Array.from(document.querySelectorAll("button"));
+const buttons = Array.from(document.querySelectorAll("#keyboard>div>button"));
 const keys = "qwertyuiop[]asdfghjkl;'zxcvbnm,./ ".split("");
 const callback = event => {
-    const y = event.key;
-    for (let h = 0; h < keys.length; h++) {
-        if (y === keys[h]) {
-            if (document.querySelector('input').checked === true) {
-                playSound(buttons[h].getAttribute("data-note"));
+
+        const y = event.key;
+        for (let h = 0; h < keys.length; h++) {
+            if (y === keys[h]) {
+                if (soundSwitch.checked === true) {
+                    console.log(buttons[h].getAttribute("data-note"));
+                    playSound(buttons[h].getAttribute("data-note"));
+                }
+                if (gameParameters.arrayTask) {
+                    inputField.textContent += y;
+                    Checking();
+                }
+                buttons[h].className = "keyboard__btn--active";
+                setTimeout(() => {
+                    buttons[h].classList.remove("keyboard__btn--active");
+                }, 300);
             }
-            inputField.textContent += y;
-            buttons[h].className = "keyboard__btn--active";
-            setTimeout(() => {
-                buttons[h].classList.remove("keyboard__btn--active");
-            }, 300);
         }
-    }
 };
 window.addEventListener("keydown", callback);
 
-const lengthText = 6;
+
 const output = document.querySelector('#js-output');
 output.textContent = localStorage.getItem('luisi');
 
+function gameTypeStart(){
+    inputLength.style.visibility = "visible";
+    inputNumber.focus();
+    start.style.visibility = "hidden";
+    let lengthText = document.querySelector("#inputNumber").value;
+    return parseInt(lengthText);
+}
+let gameParameters = {};
+
 function gameType() {
-    document.querySelector("#start").style.visibility = "hidden";
-    document.querySelector("#user").style.visibility = "visible";
-    document.querySelector("#taskText").style.visibility = "visible";
-    document.getElementById("taskText").innerHTML = `Please, print the same letters: <b> ${arrayTask}</b>`;
-    let seconds = 0;
-    let seconds_timer_id = setInterval(function () {
-        if (seconds >= 0 && inputField.textContent.length !== lengthText) {
-            seconds++;
-            if (seconds < 10) {
-                seconds = "0" + seconds;
+    let lengthText = gameTypeStart();
+    let arrayTask = makeSoloText(lengthText);
+    gameParameters.arrayTask = arrayTask;
+    if (lengthText > 0 && lengthText < 30) {
+        inputLength.style.visibility = "hidden";
+        user.style.visibility = "visible";
+        taskText.style.visibility = "visible";
+        taskText.innerHTML = `Please, print the same letters: <b> ${arrayTask}</b>`;
+
+        let seconds = 0;
+        let seconds_timer_id = setInterval(function () {
+            if (seconds >= 0 && inputField.textContent.length !== lengthText) {
+                seconds++;
+                if (seconds < 10) {
+                    seconds = "0" + seconds;
+                }
+                document.querySelector(".seconds").textContent = seconds;
             }
-            document.querySelector(".seconds").textContent = seconds;
-        }
-        if (inputField.textContent.length >= lengthText) {
-            if (arrayTask === inputField.textContent) {
-                clearInterval(seconds_timer_id);
-                let result = Math.round(lengthText / seconds * 100) / 100;
-                document.querySelector("#highScore").style.visibility = "visible"
-                document.querySelector("#highScore").innerHTML = `Your result is <b>${result} correct letters per second`;
-                if (localStorage.getItem('luisi') < result || localStorage.getItem('luisi') === null) {
-                    localStorage.setItem('luisi', result);
+             if (inputField.textContent.length >= lengthText) {
+                if (arrayTask === inputField.textContent) {
+                    clearInterval(seconds_timer_id);
+                    let result = Math.round(lengthText / seconds * 100) / 100;
+                    highScore.style.visibility = "visible";
+                    highScore.innerHTML = `Your result is <b>${result} correct letters per second`;
+                    if (localStorage.getItem('luisi') < result || localStorage.getItem('luisi') === null) {
+                        localStorage.setItem('luisi', result);
+                    }
+                }
+                else {
+                    inputField.textContent = "";
                 }
             }
-            else {
-                document.querySelector("#inputField").textContent = "";
-            }
-        }
-    }, 1000);
-    return seconds_timer_id;
-    makeSoloText(lengthText);
+        }, 1000);
+        return seconds_timer_id;
+        makeSoloText(lengthText);
+    }
+    else{
+        alert(`ATTENTION! You can choose the number from 0 to 30 only!`);
+    }
 }
 
-let randomKeys = keys.slice(0);
-
-function makeSoloText() {
+function makeSoloText(textLength) {
+    let randomKeys = keys.slice(0);
     randomKeys.sort(function () {
         return 0.5 - Math.random();
     });
     let arrayTask = [];
-    for (let k = 0; k < lengthText; k++) {
+    for (let k = 0; k < textLength; k++) {
         arrayTask.push(randomKeys[k]);
     }
     arrayTask = arrayTask.join("");
     return arrayTask;
 }
 
-let arrayTask = makeSoloText();
+let errors = 0;
+let counter = 0;
+function Checking() {
+    let lengthText = gameTypeStart();
+    let arrayTask = gameParameters.arrayTask;
+    inputLength.style.visibility = "hidden";
 
+        if (inputField.textContent[inputField.textContent.length - 1] === arrayTask[inputField.textContent.length - 1]) {
+            counter++;
+        }
+        else {
+            start.style.visibility = "visible";
+            start.innerHTML = "ERROR!";
+            start.style.color = "red";
+            inputField.textContent = inputField.textContent.slice(0, -1);
+            errors++;
+        }
 
+        if (inputField.textContent.length === arrayTask.length && errors != 0){
 
+            start.style.visibility = "visible";
+            start.innerHTML = `You've done ${errors} errors `;
+            start.style.color = "red";
 
-
-
-
-
-
-
-
+        }
+        else if (counter === arrayTask.length){
+            start.style.visibility = "visible";
+            start.innerHTML = `Super! No mistakes at all! `;
+            start.style.color = "red";
+        }
+      return errors;
+}
