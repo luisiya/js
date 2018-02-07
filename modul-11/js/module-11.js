073141b5fe7a46fcc2f8be8b2ff96d393121536e
Kravchenko Luidmila <Luisiya@gmail.com>
2018-02-07 15:14:49 +0200

    modul-11_last

--- modul-11/js/module-11.js
+++ modul-11/js/module-11.js
@@ -1,5 +1,7 @@
 let container = document.createElement("div");
 container.setAttribute("id", "fetchContent");
+container.style.fontSize = "30px";
+container.style.margin = "0 0 0 20px";
 document.body.appendChild(container);
 const getUser = (users) => {
     container.innerHTML = "";
@@ -8,67 +10,129 @@ const getUser = (users) => {
             container.innerHTML += `<div class = "${users.id}">
   ID:<b>${users.id}</b><br>
   Name:<b>${users.name}</b><br>
-  Score:<b>${users.score}</b><br>
+  Score:<b>${users.score}</b><br><br>
 </div>`;
-
-
         }
     );
-
 }
 
 function clearData() {
     fetchContent.innerHTML = "";
-    jsbtn.textContent = "getUsers";
-    jsbtn.setAttribute("onclick", "getUsers()");
-
+    getBtn.textContent = "getUsers";
+    getBtn.setAttribute("onclick", "getUsers()");
 }
 
 function getUsers() {
     fetch("http://fecore.net.ua/rest/")
-
         .then(response => {
 
             if (response.ok) return response.json();
             throw new Error("Error fetching data");
         })
         .then(data => {
-
-            console.log(data);
             getUser(data);
-            jsbtn.textContent = "CLEAR";
-            jsbtn.setAttribute("onclick", "clearData()");
-
+            getBtn.textContent = "CLEAR";
+            getBtn.setAttribute("onclick", "clearData()");
         })
 }
 
 
-let userDate = {};
+let userData = {};
 
-function setUser() {
-
-    jsbtn4.style.visibility = "hidden";
-    jsbtn.style.visibility = "hidden";
-    jsbtn3.style.visibility = "hidden";
+function addUser() {
+    setBtn.textContent = "Post";
+    setBtn.setAttribute("onclick", "validateAddUserFields(userData)");
+    updateBtn.style.visibility = "hidden";
+    getBtn.style.visibility = "hidden";
+    removeBtn.style.visibility = "hidden";
     inputLine.style.visibility = "visible";
+    inputLine2.style.visibility = "visible";
     document.querySelector("#nameInput").focus();
-    jsbtn2.textContent = "Next";
+}
 
-    if (nameInput.value != "") {
-        userDate.name = nameInput.value;
-        inputLine2.style.visibility = "visible";
+function validateAddUserFields(userData) {
+    if (nameInput.value != "" && scoreInput.value != "") {
+        userData.name = nameInput.value;
         document.querySelector("#scoreInput").focus();
-        jsbtn2.textContent = "POST";
+        inputLine2.style.visibility = "visible";
+        userData.score = scoreInput.value;
+        inputLine.style.visibility = "hidden";
+        inputLine2.style.visibility = "hidden";
+        postNewNameScore(userData);
+    }
+    else if (nameInput.value === "") {
+        alert(`Please, write the NAME`);
+    }
+    else {
+        alert(`Please, write the SCORE`)
+    }
+}
+
+const postNewNameScore = (userData) => {
+    setBtn.textContent = "Add user";
+    updateBtn.style.visibility = "visible";
+    getBtn.style.visibility = "visible";
+    removeBtn.style.visibility = "visible";
+    setBtn.setAttribute("onclick", "addUser()");
+    fetch("http://fecore.net.ua/rest/",
+        {
+            method: "POST",
+            body: JSON.stringify(userData)
+        })
+        .then(response => {
+            if (response.ok) return response.json();
+            throw new Error("Error fetching data");
+        })
+
+        .catch(function (err) {
+            console.log('Fetch Error :-S', err);
+        });
+    nameInput.value = "";
+    scoreInput.value = "";
+};
+
+
+
+function sendUpdate() {
+    setBtn.style.visibility = "visible";
+    getBtn.style.visibility = "visible";
+    removeBtn.style.visibility = "visible";
+    updateBtn.textContent = "upDate";
+    updateBtn.setAttribute("onclick", "updateUser()");
+    fetch("http://fecore.net.ua/rest/", {
+        method: "PUT",
+        body: JSON.stringify({
+            id: userUpdate.id,
+            name: userUpdate.name,
+            score: userUpdate.score
+        })
+    })
+        .then(response => {
+            if (response.ok) return response.json();
+            throw new Error("Error fetching data");
+        })
+
+        .catch(function (err) {
+            console.log('Fetch Error :-S', err);
+        });
+    nameInput.value = "";
+    scoreInput.value = "";
+}
+
+let userUpdate = {};
+
+function setNewUser() {
+    if (nameInput.value != "") {
+        userUpdate.name = nameInput.value;
+        document.querySelector("#scoreInput").focus();
 
         if (scoreInput.value != "") {
-            userDate.score = scoreInput.value;
-            console.log(userDate);
-            jsbtn2.setAttribute("onclick", addUser(userDate));
-            jsbtn4.style.visibility = "visible";
-            jsbtn.style.visibility = "visible";
-            jsbtn3.style.visibility = "visible";
-            jsbtn2.textContent = "addUsers";
-
+            userUpdate.score = scoreInput.value;
+            sendUpdate();
+            inputLine.style.visibility = "hidden";
+            inputLine2.style.visibility = "hidden";
+            titleInput.style.visibility = "hidden";
+            idSelect.style.visibility = "hidden";
         }
         else {
             alert(`Plese, write the SCORE`);
@@ -77,73 +141,6 @@ function setUser() {
     else {
         alert(`Please, write the NAME`)
     }
-}
-
-const addUser = (userDate) => {
-    fetch("http://fecore.net.ua/rest/",
-        {
-            method: "POST",
-            body: JSON.stringify(userDate)
-        })
-        .then(
-            function (response) {
-                if (response.status !== 200) {
-                    console.log('Looks like there was a problem. Status Code: ' +
-                        response.status);
-                    return;
-                }
-                // Examine the text in the response
-                response.json().then(function (data) {
-                    console.log(data);
-                    inputLine.style.visibility = "hidden";
-                    inputLine2.style.visibility = "hidden";
-                });
-            }
-        )
-        .catch(function (err) {
-            console.log('Fetch Error :-S', err);
-        });
-}
-
-
-function removeUser(userId) {
-    return fetch("http://fecore.net.ua/rest/" + userId, {
-        method: 'delete'
-    })
-        .then(response => response.json());
-}
-
-function update(userId) {
-    fetch("http://fecore.net.ua/rest/", {
-        method: "PUT",
-        body: JSON.stringify({
-            id: userId,
-            name: "Java",
-            score: "10004"
-        })
-    })
-        .then(
-            function (response) {
-                if (response.status !== 200) {
-                    console.log('Looks like there was a problem. Status Code: ' +
-                        response.status);
-                    return;
-                }
-                // Examine the text in the response
-                response.json().then(function (data) {
-                    console.log(data);
-                });
-            }
-        )
-        .catch(function (err) {
-            console.log('Fetch Error :-S', err);
-        });
-}
-
-function chooseId() {
-        let userId = document.querySelector("select");
-        userId = userId.options[userId.selectedIndex].value;
-        update(userId);
 }
 
 container2 = document.createElement("select");
@@ -161,30 +158,82 @@ function getUserId(users) {
                </option>`;
         }
     );
-    jsbtn4.innerHTML = "next";
-    jsbtn4.setAttribute("onclick", "chooseId()");
+    let userId = document.querySelector("select");
+    userId = userId.options[userId.selectedIndex].value;
+    userUpdate.id = userId;
+    updateBtn.innerHTML = "Next";
+
+    inputLine.style.visibility = "visible";
+    document.querySelector("#nameInput").focus();
+    inputLine2.style.visibility = "visible";
+
+    updateBtn.setAttribute("onclick", "setNewUser()");
 }
 
 function updateUser() {
+    getBtn.style.visibility = "hidden";
+    setBtn.style.visibility = "hidden";
+    removeBtn.style.visibility = "hidden";
     fetch("http://fecore.net.ua/rest/")
         .then(response => {
             if (response.ok) return response.json();
             throw new Error("Error fetching data");
         })
         .then(data => {
-//            console.log(data);
             getUserId(data);
-            result.style.visibility = "visible";
-            result.innerHTML = "Choose ID for update";
+            titleInput.style.visibility = "visible";
+            titleInput.innerHTML = "Choose ID for update";
         })
 }
 
 
 
+let userId;
+
+function deleteUserFromData(userId) {
+    container2.style.visibility = "hidden";
+    titleInput.style.visibility = "hidden";
+    userId = document.querySelector("select");
+    userId = userId.options[userId.selectedIndex].value;
+    removeBtn.setAttribute("onclick", "removeUser()");
+    removeBtn.innerHTML = "Delete";
+    getBtn.style.visibility = "visible";
+    setBtn.style.visibility = "visible";
+    updateBtn.style.visibility = "visible";
+    return fetch("http://fecore.net.ua/rest/" + userId, {
+        method: 'delete'
+    })
+        .then(response => response.json());
+}
 
 
+function getListId(users) {
+    container2.style.visibility = "visible";
+    container2.innerHTML = "";
+    users.map((users) => {
+            container2.innerHTML += `<option value = "${users.id}">
+                <b>${users.id}</b><br>
+ 
+               </option>`;
+        }
+    );
+    removeBtn.innerHTML = "Remove";
+    removeBtn.setAttribute("onclick", "deleteUserFromData(userId)");
+}
 
+function removeUser() {
+    getBtn.style.visibility = "hidden";
+    setBtn.style.visibility = "hidden";
+    updateBtn.style.visibility = "hidden";
+    fetch("http://fecore.net.ua/rest/")
+        .then(response => {
+            if (response.ok) return response.json();
+            throw new Error("Error fetching data");
+        })
+        .then(data => {
 
-
-
-
+            getListId(data);
+            titleInput.style.visibility = "visible";
+            titleInput.innerHTML = "Choose ID for remove";
+        })
+}
