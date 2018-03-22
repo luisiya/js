689c599144ad8e42ba30c4e5ace5f10242174068
Kravchenko Luidmila <Luisiya@gmail.com>
2018-03-22 12:27:11 +0200

    modul_15_

--- modul-15/js/module-15.js
+++ modul-15/js/module-15.js
@@ -8,15 +8,14 @@ class Timer {
     constructor(startTime, stopTime){
         this.startTime = startTime;
         this.stopTime = stopTime;
-        this.timeInterval = this.stopTime - this.startTime;
-    }
+        this.Interval = this.stopTime - this.startTime;
+         }
     static timeToNY(today, newYear){
         let oneDay = 24*60*60*1000;
         let countDays = Math.round(Math.abs((today.getTime() - newYear.getTime())/(oneDay)));
         console.log(`${countDays} days left until NY-2019`);
     }
     start(){
-
         this.startTime = seconds;
         stopWatch.startTime = this.startTime;
         let seconds_timer_id = setInterval(function () {
@@ -29,23 +28,20 @@ class Timer {
             }
             if(elementIsClicked === true){
                 clearInterval(seconds_timer_id);
-                this.stopTime = seconds;
-                stopWatch.stopTime = this.stopTime;
-                stopWatch.timeInterval = stopWatch.stopTime - stopWatch.startTime;
                 elementIsClicked = false;
                }
         }, 1000);
-
     };
     stop(){
-          elementIsClicked = true;
-          this.getTime();
+        this.stopTime = seconds;
+        this.Interval = this.stopTime - this.startTime;
+        elementIsClicked = true;
+        this.getTime();
         }
     getTime(){
-        console.log(`The inerval = ${stopWatch.timeInterval} seconds`);
-        return this.timeInterval;
+        console.log(`The inerval = ${this.Interval} seconds`);
     }
-    }
+}
 
 let timerFirst = new Timer(10,100);
 console.log(timerFirst);
@@ -54,5 +50,5 @@ let timerSecond = new Timer(5,55);
 console.log(timerSecond);
 
 let stopWatch = new Timer(0,0);
-console.log(stopWatch);
+
 Timer.timeToNY(today, newYear);
